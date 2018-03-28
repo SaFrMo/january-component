b8ec549eef6a50f16e88dedfc76e62cdb8580706
Sander Moolin <sandermoolin@gmail.com>
2018-03-28 09:03:24 -0400

    Initial setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import AnimatingLogo from '@/components/AnimatingLogo'
 
 Vue.config.productionTip = false
 
+// components
+Vue.component('animating-logo', AnimatingLogo)
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App)
+	el: '#app',
+	render: h => h(App)
 })
